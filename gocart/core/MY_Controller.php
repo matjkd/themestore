f7bc7ca72abf532fb3059947d90b9915df73641b
Mat Sadler <mat@redstudio.co.uk>
2013-05-07 13:44:54 +0100

    Let's start with GoCart

---
+++ gocart/core/MY_Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+class Admin_Controller extends CI_Controller 
+{
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->library('auth');
+		$this->auth->is_logged_in(uri_string());
+		
+		//load the base language file
+		$this->lang->load('admin_common');
+		$this->lang->load('goedit');
+	}
+}
